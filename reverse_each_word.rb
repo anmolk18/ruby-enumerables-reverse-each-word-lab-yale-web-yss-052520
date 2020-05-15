c306b653195964c0f9a68d979c1796e8977e4136
Anmolpreet Kaur <anmolpreet.kaur@yale.edu>
2020-05-15 00:13:41 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -6,4 +6,11 @@ def reverse_each_word (string)
   new_array.join(" ")
   new_array
 end 
+#couldn't get it to work with each
 
+def reverse_each_word(string)
+  array = string.split(" ")
+  new_array = []
+  new_array = array.collect {|word| word.reverse }
+  new_array.join(" ")
+end
